63921779033bb092e33aa6841a624793b8081a7c
Miaoday <Starryvero@gmail.com>
2023-12-10 22:00:19 +0100

    trash button update

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,7 +1,6 @@
-// Get the DataSource Categories from the server API
+// Get the Categories from API
 async function getApiCategories() {  
    try {
-      // get the Categories
       const getApiCategories = await fetch('http://localhost:5678/api/categories');
       if (!getApiCategories.ok) {
         throw Error('Network response was not ok');
@@ -16,11 +15,9 @@ async function getApiCategories() {
       console.log('Fetch categories error:', error)
    }
 }
-
-// Get the DataSource Works from the server API
+// Get the DataSource Works from API
 async function getApiWorks() {
    try {
-   // get the Works
       const getApiWorks = await fetch('http://localhost:5678/api/works');
       if (!getApiWorks.ok) {
          throw Error('Works request failed');
@@ -35,7 +32,6 @@ async function getApiWorks() {
       console.log('Fetch works error:', error)
    }
 }
-
 // For treating the callback data
 async function fetchData() {
    try {
@@ -44,158 +40,21 @@ async function fetchData() {
      // collecting all API Datas therfore retrieving by other function
      updateGallery(works); // calling updateGallery function
      filtreByCategories(categories,works); // calling filreByCategories function 
+   //   displayModal(works);
      return { categories, works };
-   } 
-   catch (error) {
+   } catch (error) {
      console.error('Error fetching data:', error);
    }
+   //   displayModal().then(works => {
+   //    console.log(works);  
+   // }).catch(error => {
+   //    console.error(error);
+   // });
 }
-// fetchData(); // calling fetchData function
+fetchData();
 
-// Import the project into the Modal Window
-let trashBin;
-const editGallery = document.getElementById('edit-modal');
-async function displayModal () {
-   try{
-      const {works} = await fetchData();  // make sure works had initiated
-      console.log(works);        
-      editGallery.innerHTML = '';  // clear actual gallery elements
-      works.forEach (work=> {  
-         const figure = document.createElement("figure");
-         const figureImg = document.createElement("img");
-         const trashIcon = document.createElement("i");
-         figure.id = work.id;
-         figureImg.src = work.imageUrl;
-         figureImg.alt = work.title;
-         figureImg.crossOrigin = "anonymous";
-         figure.className = work.categoryId;
-         trashIcon.className += "fa-regular fa-trash-can";   
-         trashIcon.setAttribute('id','trash');
-         // attatched figure into the gallery
-         figure.appendChild(figureImg);
-         figure.appendChild(trashIcon);
-         editGallery.appendChild(figure);
-      }); 
-      return works; 
-   } catch (error) {
-      console.log(error);
-      return [];
-   }
-}
-
-displayModal().then(works => {
-   console.log(works);
-
-   // Delete the project with trashbin
-   const trashBtns = document.querySelectorAll('.fa-trash-can');
-   console.log(trashBtns); 
-   [...trashBtns].forEach(trash=>{
-      trash.addEventListener ('click', function(e) {
-         try {
-            let figure = this.parentNode;
-            let projectId = figure.id;
-            console.log(projectId);
-            async function getDelet() {
-               await fetch (`http://localhost:5678/api/works/${projectId}`, {
-               method: 'DELETE',
-               headers: {
-                  Authorization: 'Bearer ${token}',
-                  'Content-Type': 'application/json'
-               }
-               });
-            }
-            getDelet.then(function (response){
-
-            if (response.status===204){
-               figure.remove();
-               // let figureElement = figure.id;
-               // let deleteFigure = document.getElementById(figureElement);          
-               // deleteFigure.remove();
-               console.log('image deleted successfully');    
-                        
-            } else if (response.status===401){
-               console.error('Unauthorized: You do not have permission to delete this image.');
-               // statusOfResponse = response.status;
-               // alertModalGallery.style.display = "flex";
-               // alertModalGallery.innerHTML =
-               // "Vous n'avez pas les autorisations pour effacer le fichier, statut " + resStatus;               
-            } else {
-               console.error('Failed to delete image:', response.status);
-               // tatusOfResponse = response.status;
-               // console.error('failed to delete image:', response.status);
-               // alertModalGallery.style.display = "flex";
-               // alertModalGallery.innerHTML =
-               // "Impossible d'effacer le fichier, problème d'accès à l'API." + resStatus;
-            } 
-            });
-            getDelet();
-
-      } catch (error){
-               console.error('Error deleting project:', error);
-            }     
-   })
-}).catch(error => {
-   console.error(error);
-});
-});
-  
-// Token status setting for the Admin page
-const adminNav = document.getElementById('admin-nav');
-const logInOut= document.getElementById('log-in-out');
-const titlePortfolio = document.querySelector('.portfolio-title');
-const modalButton = document.getElementById('modal-btn');
-const filtreBtn = document.querySelectorAll('.filtreBtn');
-
-let token = localStorage.getItem('token');
-console.log('Token value:', token)
-
-if (token){
-   adminNav.style.display = "block";
-   modalButton.style.visibility = "visible";
-   logInOut.innerHTML = "logout";   
-}
-
-function logOut() {
-
-   if(token===null){
-      console.log('Token value is null')
-      window.location.replace('./login.html');
-   } else {
-      localStorage.clear();
-      window.location.replace('./index.html');
-   }
-};
-
-logInOut.addEventListener("click",(e)=>{
-   logOut();
-
-})
-
-const gallery = document.querySelector(".gallery");
-const galleryId = document.getElementById("galleryid");
-
-// Update portfolio works & gallery
-function updateGallery (works) {
-   gallery.innerHTML = '';  // clear actual gallery elements
-   works.forEach(work=> {
-      const figure = document.createElement("figure");
-      const figureImg = document.createElement("img");
-      const figcaption = document.createElement("figcaption");  
-      figure.id = work.id;
-      figureImg.src = work.imageUrl;
-      figureImg.alt = work.title;
-      figureImg.crossOrigin = "anonymous";
-      figure.className = work.categoryId;
-      figcaption.innerText = work.title;
-
-      // attatched figure into the gallery
-      figure.appendChild(figureImg);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
-   });
-}
-
-// Classify the works
+// HomePageSetting
+// Classify the works with the categories buttons
 function filtreByCategories (categories,works) {    
    const classment = document.getElementById("filtres"); 
    // create the Button 'Tous'
@@ -240,6 +99,62 @@ function filtreByCategories (categories,works) {
    });
 }
 
+// Update dynamic portfolio works in gallery
+const gallery = document.querySelector(".gallery");
+const galleryId = document.getElementById("galleryid");
+
+function updateGallery (works) {
+   gallery.innerHTML = '';  // clear actual gallery elements
+   works.forEach(work=> {
+      const figure = document.createElement("figure");
+      const figureImg = document.createElement("img");
+      const figcaption = document.createElement("figcaption");  
+      figure.id = work.id;
+      figureImg.src = work.imageUrl;
+      figureImg.alt = work.title;
+      figureImg.crossOrigin = "anonymous";
+      figure.className = work.categoryId;
+      figcaption.innerText = work.title;
+
+      // attatched figure into the gallery
+      figure.appendChild(figureImg);
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+   });
+}
+
+// Token status setting for Admin page
+const adminNav = document.getElementById('admin-nav');
+const logInOut= document.getElementById('log-in-out');
+const titlePortfolio = document.querySelector('.portfolio-title');
+const modalButton = document.getElementById('modal-btn');
+const filtreBtn = document.querySelectorAll('.filtreBtn');
+
+let token = localStorage.getItem('token');
+console.log('Token value:', token)
+
+if (token){
+   adminNav.style.display = "block";
+   modalButton.style.visibility = "visible";
+   logInOut.innerHTML = "logout";   
+}
+
+function logOut() {
+
+   if(token===null){
+      console.log('Token value is null')
+      window.location.replace('./login.html');
+   } else {
+      localStorage.clear();
+      window.location.replace('./index.html');
+   }
+};
+
+logInOut.addEventListener("click",(e)=>{
+   logOut();
+
+})
+
 // Modal window settings
 const modalWindow = document.querySelector('.modal-window');
 const modalWrapper = document.querySelector('.modal-wrapper');
@@ -252,6 +167,80 @@ function toggleModal () {
    modalWindow.classList.toggle("active")
 }
 
+// Import the project into the Modal Window
+const editModal = document.getElementById('edit-modal');
+async function displayModal () {
+   try{
+      const {works} = await fetchData();  // make sure works had initiated
+      console.log(works);        
+      editModal.innerHTML = '';  // clear actual gallery elements
+      works.forEach (work=> {  
+         const figure = document.createElement("figure");
+         const figureImg = document.createElement("img");
+         const trashBtn = document.createElement("i");
+         figure.id = work.id;
+         figureImg.src = work.imageUrl;
+         figureImg.alt = work.title;
+         figureImg.crossOrigin = "anonymous";
+         figure.className = work.categoryId;
+         trashBtn.className += "fa-regular fa-trash-can";   
+         trashBtn.setAttribute('id', work.id);
+         // attatched figure into the gallery
+         figure.appendChild(figureImg);
+         figure.appendChild(trashBtn);
+         editModal.appendChild(figure);
+
+         document.querySelectorAll('.fa-trash-can').forEach((trashBtn)=> {                
+            trashBtn.addEventListener('click', deletWork)   
+            // console.log(trashBtn);             
+            // console.log('Click trashButton')                   
+         }); 
+      return works; 
+      }); 
+   }catch (error) {
+      console.log(error);
+      return [];  
+}
+}
+displayModal().then(works => {
+   console.log(works);  
+}).catch(error => {
+   console.error(error);
+});
+
+// Delete the project with trashbin
+async function deletWork(event) {
+   id = event.target.id;
+   try{
+      const response =
+      await fetch (`http://localhost:5678/api/works/${id}`, {
+      method: 'DELETE',
+      headers: {
+         Authorization: `Bearer ${token}`,
+         'Content-Type': 'application/json'
+      }   
+      });
+  
+   if (response.ok){
+      // figure.remove();
+      console.log('Item deleted successfully');  
+      displayModal();
+      fetchData();  
+               
+   } else if (response.status===401){
+      console.error('Unauthorized: You do not have permission to delete this image.');            
+   } else if(response.status===500){
+      console.error('Unexpected Behavior:', response.status);
+   } else {
+      console.error('Delete failed :', response.status);
+   }
+
+   } catch (error) {
+      console.error('Error),', error);
+   }
+}
+
+// setting Vierge Modal window
 const addProject = document.getElementById('add-photo');
 // ajouterPhoto.addEventListener("click",(event) =>{
 // })
